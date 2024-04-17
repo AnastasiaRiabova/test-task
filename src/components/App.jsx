7012846f9a204ac23635c85805cc7b680a75a634
AnastasiaRiabova <riabova321@gmail.com>
2024-04-17 19:08:03 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import classes from './App.module.css';
+
+export const App = () => {
+
+  return (
+    <main>
+      <div className={classes.container}>
+        Project
+      </div>
+    </main>
+  );
+};
